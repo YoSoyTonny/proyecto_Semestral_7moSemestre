a8be7b57dbe144a11d62946e9e6e78d7d8077a46
Tonny <itonygame@gmail.com>
2019-11-15 13:59:39 -0700

    Initial Commit

---
+++ ProyectoEmilianoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoEmilianoApp
+//
+//  Created by Tonny Gammer on 15/11/19.
+//  Copyright © 2019 Tonny Gammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
